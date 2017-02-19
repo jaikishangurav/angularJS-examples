65e899ba4dd62120260c5b69609a8bcb14c2b639
jaikishangurav <jackmail.47@gmail.com>
2017-02-19 16:36:21 +0530

    added REST API calls to add, update and fetch contacts

--- angular-services/ContactApp/contact.controller.js
+++ angular-services/ContactApp/contact.controller.js
@@ -3,17 +3,57 @@
 
     app.controller("ContactCtrl", ContactCtrl);
 
-    function ContactCtrl(ContactDataSvc,AppDataServiceSvc) {
-    this.contacts = ContactDataSvc.contacts;
+    function ContactCtrl(ContactDataSvc, AppDataServiceSvc) {
+        var self = this;
+        self.editMode = false;
+        self.addMode=false;
+        self.contacts = [];
+        ContactDataSvc.getContacts().then(function (data) {
+            self.contacts = data;
+        });
 
-    
-    this.appTitle=AppDataServiceSvc.name;
+        this.appTitle = AppDataServiceSvc.name;
 
-    this.selectedContact = this.contacts[0];
+        this.selectedContact = this.contacts[0];
 
-    this.selectContact = function (index) {
-        this.selectedContact = this.contacts[index];
+        this.selectContact = function (index) {
+            this.selectedContact = this.contacts[index];
+            self.successMessage=undefined;
+            self.errorMessage=undefined;
+        }
+
+        this.toggleEditMode = function () {
+            self.editMode = !self.editMode;
+        }
+
+        this.saveUser = function () {
+            this.toggleEditMode();
+            var userData = this.selectedContact;
+            if(self.addMode){
+                ContactDataSvc.createUser(userData).
+                then(function () {
+                    self.successMessage = "Data successfully updated";
+                },function(){
+                    self.errorMessage="There was an error. Please try again later.";
+                });
+                self.addMode=false;
+            }else{
+            ContactDataSvc.saveUser(userData).
+                then(function () {
+                    self.successMessage = "Data successfully updated";
+                },function(){
+                    self.errorMessage="There was an error. Please try again later.";
+                });
+            }
+        }
+
+        this.addContact=function(){
+            self.addMode=true;
+            self.selectedContact={
+                "id":new Date().toDateString()
+            }
+            self.editMode=true;
+        }
     }
-}
-    
+
 })();
